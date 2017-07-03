bad53ad6ac86f7e20577d7a5397143b6a13eadee
VarunKurumella <varunkurumella@gmail.com>
2017-07-03 16:03:46 +0530

    New Commit

--- KeywordDrivenUsingFillo/src/com/sampleExcelReadingFillo/MethodCall.java
+++ KeywordDrivenUsingFillo/src/com/sampleExcelReadingFillo/MethodCall.java
@@ -3,12 +3,12 @@ package com.sampleExcelReadingFillo;
 import java.lang.reflect.Method;
 
 public class MethodCall {
+	//Method Invoke
 	public static void methodInvoke(String clsName,String methodName) throws ClassNotFoundException, ReflectiveOperationException, IllegalAccessException{
 		@SuppressWarnings("rawtypes")
 		Class c = Class.forName(clsName);
 		Object o = c.newInstance();
 		try {
-			//Method Invoke
 			Method m = MethodInvoke.class.getMethod(methodName, null);
 			m.invoke(o,null);
 		} catch (IllegalArgumentException e) {
